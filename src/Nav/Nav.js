09907b09502563f3322499927e2dc40123fc7008
Mayur7030 <117289178+Mayur7030@users.noreply.github.com>
2023-09-21 11:58:37 +0530

    Add files via upload

---
+++ src/Nav/Nav.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react'
+import './Nav.css'
+
+function Nav() {
+
+    const [show, handleShow] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                handleShow(true)
+            } else handleShow(false)
+        });
+        return () => {
+            window.removeEventListener('scroll')
+        };
+    }, []);
+
+    return (
+        <div className={`nav ${show && "nav_black"}`}>
+            {/* Netflix logo */}
+            <img className="nav_logo" src="https://www.logo.wine/a/logo/Netflix/Netflix-Logo.wine.svg" alt="Netflix logo"/>
+            <p className='creater'>--By Mayur</p>
+
+            {/* Avatar logo */}
+            <img className="nav_avatar" src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="Netflix avatar"/>
+
+
+        </div>
+    )
+}
+
+export default Nav
